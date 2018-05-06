2d4c93bbb00162e565085a138700879446aacf31
Matt McKnett <mmcknett@gmail.com>
2018-05-06 13:11:24 -0700

    Add a post method to the serverless config.

---
+++ api/post-guests.js
@@ -0,0 +1,36 @@
+const isCodeValid = require('../rsvp-logic/is-code-valid');
+
+module.exports.postGuests = async (event, context, callback) => {
+    console.log('Updating guests...');
+
+    const { Authorization: authorization } = event.headers;
+    const prefix = 'Bearer ';
+
+    let statusCode = 200;
+    let message;
+
+    if (!authorization || !authorization.startsWith(prefix)) {
+        statusCode = 400;
+        message = 'Invalid authorization scheme.';
+    } else {
+        const inviteCode = authorization.slice(prefix.length);
+
+        if (await isCodeValid(inviteCode)) {
+            statusCode = 200;
+            message = 'Token valid, but did nothing.';
+        } else {
+            statusCode = 401;
+            message = 'Invalid token.';
+        }
+    }
+
+    const response = {
+        statusCode,
+        body: JSON.stringify({
+            message,
+            requestBody: event.body
+        }),
+    };
+
+    callback(null, response);
+};
